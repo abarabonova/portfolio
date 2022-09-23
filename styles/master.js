6f1096c4406a683c7d28848e2220fb2a96a660b8
abarabonova <abarabonova@mail.ru>
2022-09-23 13:02:49 +0300

    new
    new

---
+++ styles/master.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(document).ready(function () {
+    $("#mouseover").mouseover(function () {
+      $(".footer__btn-images").css("background", "url(./img/new-1/inst1.jpg)");
+    });
+  });
+})(jQuery);
